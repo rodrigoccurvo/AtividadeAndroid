e0f61aa58543a3e7f98c833edf7941d697a12235
Rodrigo Coin Curvo <rodrigoccurvo@gmail.com>
2015-06-10 17:22:04 -0400

    Tarefa 2 - Lista de itens

--- app/src/main/java/com/example/rodrigo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rodrigo/myapplication/MainActivity.java
@@ -1,17 +1,49 @@
 package com.example.rodrigo.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
+@SuppressWarnings("deprecation")
 public class MainActivity extends ActionBarActivity {
+
+    private ListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] dados = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+
+        List<String> previsao = new ArrayList<>(Arrays.asList(dados));
+
+        mAdapter = new ArrayAdapter<>(
+                getApplicationContext(), // Contexto atual
+                R.layout.item_lista, // Nome do ID do layout
+                R.id.item_texto, // ID do TextView a ser preenchido
+                previsao);
+
+        ListView listView = (ListView) findViewById(R.id.lista_principal);
+        listView.setAdapter(mAdapter);
+
     }
 
     @Override
